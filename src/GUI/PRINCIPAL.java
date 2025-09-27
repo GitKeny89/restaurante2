9060bfbf7beb91e74974dfcae1d2a473478fbcb3
GitKeny89 <kenyesaupajarito@gmail.com>
2025-09-26 22:54:11 -0600

    update

---
+++ src/GUI/PRINCIPAL.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+
+public class PRINCIPAL {
+
+    public static void main(String[] args) {
+      FRM_INICIO formulario  = new FRM_INICIO();
+      formulario.setVisible(true);
+        
+    }
+    
+}
